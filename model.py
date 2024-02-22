8f21839c6ea2bbd77a229538d3932de259e18ea5
Yichuan <1758783038@qq.com>
2024-02-22 15:31:11 -0500

    Commit

--- model.py
+++ model.py
@@ -13,7 +13,30 @@ from utils import customlog
 
 
 
+class LayerNormNet(nn.Module):
+    def __init__(self, hidden_dim, out_dim, device, dtype, drop_out=0.1):
+        super(LayerNormNet, self).__init__()
+        self.hidden_dim1 = hidden_dim
+        self.out_dim = out_dim
+        self.drop_out = drop_out
+        self.device = device
+        self.dtype = dtype
 
+        self.fc1 = nn.Linear(320, hidden_dim, dtype=dtype, device=device)
+        self.ln1 = nn.LayerNorm(hidden_dim, dtype=dtype, device=device)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim,
+                             dtype=dtype, device=device)
+        self.ln2 = nn.LayerNorm(hidden_dim, dtype=dtype, device=device)
+        self.fc3 = nn.Linear(hidden_dim, out_dim, dtype=dtype, device=device)
+        self.dropout = nn.Dropout(p=drop_out)
+
+    def forward(self, x):
+        x = self.dropout(self.ln1(self.fc1(x)))
+        x = torch.relu(x)
+        x = self.dropout(self.ln2(self.fc2(x)))
+        x = torch.relu(x)
+        x = self.fc3(x)
+        return x
 
 
 def initialize_PromptProtein(pretrain_loc, trainable_layers):
