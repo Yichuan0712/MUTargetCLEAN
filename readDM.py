b76e212e797816d62f099f5c3869fbc1b6d483a8
Yichuan <1758783038@qq.com>
2024-02-26 13:39:27 -0500

    Commit

---
+++ readDM.py
@@ -0,0 +1,12 @@
+import json
+
+def read_data_from_file(filename):
+    with open(filename, 'r') as file:
+        data = file.read()
+        dictionary = json.loads(data)
+    return dictionary
+
+# 假设你的文件名为"data.txt"
+filename = "dm.txt"
+data_dict = read_data_from_file(filename)
+print(data_dict)
