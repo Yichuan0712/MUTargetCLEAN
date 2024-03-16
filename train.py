aa8c3869e5538921b8f453c37160704208c2297b
Yichuan <1758783038@qq.com>
2024-03-16 00:16:52 -0400

    Commit

--- train.py
+++ train.py
@@ -76,27 +76,18 @@ def train_loop(tools, configs, warm_starting):
     tools['net'].train().to(tools['train_device'])
     for batch, (id_tuple, id_frag_list_tuple, seq_frag_list_tuple, target_frag_nplist_tuple, type_protein_pt_tuple, sample_weight_tuple, pos_neg) in enumerate(tools['train_loader']):
         if configs.supcon.apply and not warm_starting and pos_neg is not None:
-            print(len(id_tuple))
-            print(len(id_frag_list_tuple))
-            print(len(pos_neg))
-            # print(len(pos_neg))
-            # print((pos_neg))
-            # for i in range(len(pos_neg[0])):
-            #     id_tuple += tuple(pos_neg[0][i][0])
-            #     id_frag_list_tuple += tuple(pos_neg[0][i][1])
-            #     seq_frag_list_tuple += tuple(pos_neg[0][i][2])
-            #     target_frag_nplist_tuple += tuple(pos_neg[0][i][3])
-            #     type_protein_pt_tuple += tuple(pos_neg[0][i][4])
-            #     sample_weight_tuple += tuple(pos_neg[0][i][5])
-            # print(len(id_tuple))
-            # for i in range(len(pos_neg[1])):
-            #     id_tuple += tuple(pos_neg[1][i][0])
-            #     id_frag_list_tuple += tuple(pos_neg[1][i][1])
-            #     seq_frag_list_tuple += tuple(pos_neg[1][i][2])
-            #     target_frag_nplist_tuple += tuple(pos_neg[1][i][3])
-            #     type_protein_pt_tuple += tuple(pos_neg[1][i][4])
-            #     sample_weight_tuple += tuple(pos_neg[1][i][5])
-            # print(len(id_tuple))
+            print(id_tuple.shape)
+            print(id_frag_list_tuple.shape)
+            pos_transformed = [[[] for _ in range(5)] for _ in range(configs.supcon.n_pos)]
+            neg_transformed = [[[] for _ in range(5)] for _ in range(configs.supcon.n_neg)]
+            for i in range(configs.train_settings.batch_size):
+                for j in range(configs.supcon.n_pos):
+                    for k in range(5):
+                        pos_transformed[j][k].append(pos_neg[i][0][j][k])
+            for i in range(configs.train_settings.batch_size):
+                for j in range(configs.supcon.n_neg):
+                    for k in range(5):
+                        pos_transformed[j][k].append(pos_neg[i][1][j][k])
             exit(0)
         id_frags_list, seq_frag_tuple, target_frag_pt, type_protein_pt = make_buffer(id_frag_list_tuple, seq_frag_list_tuple, target_frag_nplist_tuple, type_protein_pt_tuple)
         with autocast():
